c634c4f6e040558d01c5d24ce4e7c1469088ab7d
Michael Ribbons <mribbons@gmail.com>
2022-06-23 10:43:16 +1000

    Implement basic sample showing lack of generic argument injections

---
+++ index.ts
@@ -0,0 +1,40 @@
+import { getType } from "tst-reflect";
+
+export default interface IService
+{
+    typedMethod<T>(a: any): void
+}
+
+export class Service implements IService
+{
+    printTypeProperties<TType>()
+    {
+        const type = getType<TType>(); // <<== get type of generic TType
+        
+        console.log(type.getProperties().map(prop => prop.name + ": " + prop.type.name).join("\n"));
+    }
+
+    typedMethod<T>(a: any): void {
+        this.printTypeProperties<T>();
+    }
+}
+
+export class MyType {
+    member: string = "hello";
+}
+
+export class Tester
+{
+    testGeneric(): void
+    {
+        var mt = new MyType();
+        var service = new Service();
+        var _interface: IService = service as IService;
+
+        service.typedMethod<MyType>({});
+        _interface.typedMethod<MyType>({});
+
+    }
+}
+
+new Tester().testGeneric();
